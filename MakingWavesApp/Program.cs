bad190f32e96a5a91e654c0dd63091a06e561ab0
Maksym Krawczyk <maksym.krawczyk@insanelab.com>
2018-04-21 18:22:52 +0200

    Adds renderer

--- MakingWavesApp/Program.cs
+++ MakingWavesApp/Program.cs
@@ -6,18 +6,52 @@ namespace MakingWavesApp
 {
     internal class Program
     {
+        private const string DateFormat = "dd.MM.yyyy";
+
         public static void Main(string[] args)
-        {
+        {   
             if (args.Length != 2)
             {
                 Console.WriteLine("Wrong arguments count.");
                 return;
             }
 
-            var dateFrom = DateTime.ParseExact(args.First(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
-            var dateTo = DateTime.ParseExact(args.Last(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            SetDateFromString(args.First(), DateFormat, out var dateFrom);
+            SetDateFromString(args.Last(), DateFormat, out var dateTo);
 
-            Console.ReadKey();
+            using (var datePeriodRenderer= new DatePeriodRenderer())
+            {
+                datePeriodRenderer.RenderPeriod(dateFrom, dateTo);
+            }
+        }
+
+        private static void SetDateFromString(string dateString, string format, out DateTime date)
+        {
+            try
+            {
+                date = DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Could not parse date. Make sure date format is {format}.");
+            }
+        }
+    }
+
+    internal class DatePeriodRenderer : IDisposable
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void RenderPeriod(DateTime dateFrom, DateTime dateTo)
+        {
+            var timeSpan = dateTo - dateFrom;
+            
+            if (timeSpan < TimeSpan.FromDays(1))
+            
+            Console.WriteLine($"{dateFrom} - {dateTo}");
         }
     }
 }
