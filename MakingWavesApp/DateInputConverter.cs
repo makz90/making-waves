e93388af1843f957daa13d5e3c077e68308f0591
Maksym Krawczyk <maksym.krawczyk@insanelab.com>
2018-04-22 21:35:24 +0200

    Moves input converting to separate file

---
+++ MakingWavesApp/DateInputConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+
+namespace MakingWavesApp
+{
+    public class DateInputConverter
+    {
+        public DateInputConverter(IReadOnlyCollection<string> args)
+        {
+            if (args.Count != 2)
+            {
+                Console.WriteLine("Wrong arguments count.");
+                return;
+            }
+            
+            var dateFrom = GetDateFromString(args.FirstOrDefault(), DatePeriodRenderer.FullDateFormat);
+            var dateTo = GetDateFromString(args.LastOrDefault(), DatePeriodRenderer.FullDateFormat);
+
+            if (dateFrom == null || dateTo == null)
+            {
+                Console.WriteLine("Date input could not be parsed.");
+                return;
+            }
+            
+            if (dateFrom >= dateTo)
+            {
+                Console.WriteLine("First date must be earlier than second one!");
+                return;
+            }
+
+            DateFrom = dateFrom;
+            DateTo = dateTo;
+        }
+
+        public DateTime? DateFrom { get; }
+        
+        public DateTime? DateTo { get; }
+
+        private static DateTime? GetDateFromString(string dateString, string format)
+        {
+            try
+            {
+                return DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($"Wrong date format provided ({dateString}). Make sure date format is [{format}]");
+                Debug.Print(fe.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occured while trying to parse date ({dateString}). Please try again.");
+                Debug.Print(e.Message);
+            }
+
+            return null;
+        }
+    }
+}
--- MakingWavesApp/Program.cs
+++ MakingWavesApp/Program.cs
@@ -1,57 +1,14 @@
-﻿using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-
-namespace MakingWavesApp
+﻿namespace MakingWavesApp
 {
     internal static class Program
     {
         public static void Main(string[] args)
-        {   
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Wrong arguments count.");
-                return;
-            }
-            
-            var dateFrom = GetDateFromString(args.FirstOrDefault(), DatePeriodRenderer.FullDateFormat);
-            var dateTo = GetDateFromString(args.LastOrDefault(), DatePeriodRenderer.FullDateFormat);
-
-            if (dateFrom == null || dateTo == null)
-            {
-                Console.WriteLine("Date input could not be parsed.");
-                return;
-            }
-            
-            if (dateFrom >= dateTo)
-            {
-                Console.WriteLine("First date must be earlier than second one!");
-                return;
-            }
+        {
+            var converter = new DateInputConverter(args);
+            if (converter.DateFrom == null || converter.DateTo == null) return;
             
             var datePeriodRenderer = new DatePeriodRenderer();
-            datePeriodRenderer.RenderPeriod(dateFrom.Value, dateTo.Value);
-        }
-
-        private static DateTime? GetDateFromString(string dateString, string format)
-        {
-            try
-            {
-                return DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
-            }
-            catch (FormatException fe)
-            {
-                Console.WriteLine($"Wrong date format provided ({dateString}). Make sure date format is [{format}]");
-                Debug.Print(fe.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"An error occured while trying to parse date ({dateString}). Please try again.");
-                Debug.Print(e.Message);
-            }
-
-            return null;
+            datePeriodRenderer.RenderPeriod(converter.DateFrom.Value, converter.DateTo.Value);
         }
     }
 }
